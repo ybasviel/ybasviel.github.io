0f16eac47ac33de7a56101ad4a71a2a944c1c180
ybasviel <calin.8082@gmail.com>
2021-04-29 12:19:55 +0900

    add link

--- works/yt_thumbnail/yt_thumbnail.js
+++ works/yt_thumbnail/yt_thumbnail.js
@@ -1,7 +1,11 @@
 const change = document.getElementById("change");
 const input = document.querySelector("input");
+
 const sdimg = document.querySelector("sd");
 const hdimg = document.querySelector("hd");
+
+const sd_link = document.getElementById("sd_link");
+const hd_link = document.getElementById("hd_link");
 
 
 let sdimg_url = "";
@@ -16,9 +20,11 @@ function rewrite()
     youtube_id = youtube_ids[youtube_ids.length-1];
 
     sdimg_url = "http://img.youtube.com/vi/" + youtube_id + "/sddefault.jpg";
+    sd_link.href = sdimg_url;
     sd.src = sdimg_url;
 
     hdimg_url = "http://img.youtube.com/vi/" + youtube_id + "/maxresdefault.jpg"
+    hd_link.href = hdimg_url;
     hd.src = hdimg_url;
 }
 
