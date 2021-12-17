e7f38ddc5919bbf33aed2edb985f579c5eaec8f1
ybasviel <67199957+ybasviel@users.noreply.github.com>
2021-12-17 09:58:59 +0900

    change blog-path

--- make.py
+++ make.py
@@ -10,18 +10,20 @@ linksforindex ="<ul class=\"url-list\">\n"
 linksforarchive = "<ul class=\"archive\">\n"
 first = True
 
-htmlfiles = glob.glob("./blog/*.html")
+htmlfiles = glob.glob("./blog/*")
 htmlfiles.sort(reverse=True)
 for url in htmlfiles:
     if url[-10:] == "index.html":
         pass
     else:
         #ファイルを開いて
-        with open(url) as file:
+        with open(url + "/index.html") as file:
             nakami = file.read()
             #titleタグからページ名を習得、頭についてる「りんりん - 」は外す
             pagename = re.sub("りんりん - ","",re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' '))))
-            date = re.sub("</div class=\"date\">.*", "",re.sub(".*<div class=\"date\">","",nakami.replace('\n',' ')))
+            date = url[-10:] #re.sub("</div class=\"date\">.*", "",re.sub(".*<div class=\"date\">","",nakami.replace('\n',' ')))
+            date = re.sub("-","/",date)
+
             linksforindex += "      <li><a href=\"." + url + "\">" + pagename + " - " + date + "</a></li>\n"
 
             if first:
@@ -44,15 +46,20 @@ with open("./blog/index.html", mode="w") as file:
     file.write(onew)
 
 
-for category in ["./works/*/*.html","./blog/*.html"]:
+for category in ["./works/*/*.html","./blog/*/*.html"]:
     files = glob.glob(category)
     for url in files:
         with open(url) as file:
             nakami = file.read()
             #titleタグからページ名を習得、頭についてる「りんりん - 」は外す
             pagename = re.sub("りんりん - ","",re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' '))))
-            linksforindex += "      <li><a href=\"" + url + "\">" + pagename + "</a></li>\n"
-            metatag = "<!--置換用タグ1-->\n    <meta property=\"og:url\" content=\"https://lnln.dev/" + re.sub("\./","",url) + "\">\n    <meta property=\"og:title\" content=\"りんりん - " + pagename + "\">\n    <!--置換用タグ2-->"
+
+            if url[-10:] == "index.html":
+                shorturl = url[:-11]
+            else:
+                shorturl = url
+            linksforindex += "      <li><a href=\"" + shorturl + "\">" + pagename + "</a></li>\n"
+            metatag = "<!--置換用タグ1-->\n    <meta property=\"og:url\" content=\"https://lnln.dev/" + re.sub("\./","",shorturl) + "\">\n    <meta property=\"og:title\" content=\"りんりん - " + pagename + "\">\n    <!--置換用タグ2-->"
 
             onew = re.sub("<!--置換用タグ1-->.*<!--置換用タグ2-->",metatag,nakami,flags=re.DOTALL)
         with open(url,mode="w") as file:
