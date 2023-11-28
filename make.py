67ec2e0086dd8322beefb5ea970bece1020a1bb4
ybasviel <67199957+ybasviel@users.noreply.github.com>
2023-11-29 02:51:06 +0900

    md-html-converter

--- make.py
+++ make.py
@@ -4,90 +4,142 @@ import glob
 import re
 import os
 from shutil import copytree, copyfile
+from pathlib import Path
+import markdown
+
 
 NUMBER_OF_LATEST_BLOG = 3
 
-links_for_index_html ='<ul class="url-list">\n'
-links_for_archive_list = '<ul class="archive">\n'
-first = True
-
-# ls blog html files
-html_files = glob.glob("./blog/*")
-html_files.sort(reverse=True)
-
-#latest3 = 0 #最新の3件のみ表示
-
-for index, url in enumerate(html_files):
-    if url[-10:] == "index.html":
-        pass
-    else:
-        #ファイルを開いて
-        with open(url + "/index.html") as file:
-            nakami = file.read()
-
-            # get title
-            pagename = re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' ')))
-            date = url[-10:]
-            date = re.sub("-","/",date)
-
-            if index <= NUMBER_OF_LATEST_BLOG:
-                links_for_index_html += f'      <li><a href=".{url}">{pagename} - {date}</a></li>\n'
-
-            if first:
-                links_for_archive_list += f'      <details><summary>{date[:-3]}</summary>\n        <li><a href=".{url}">{pagename} - {date}</a></li>\n'
-                first = False
-            elif former != date[:-3]:
-                links_for_archive_list += f'      </details>\n      <details><summary>{date[:-3]}</summary>\n        <li><a href=".{url}">{pagename} - {date}</a></li>\n'
-            else:
-                links_for_archive_list += f'        <li><a href=".{url}">{pagename} - {date}</a></li>\n'
-            former = date[:-3]
-
-links_for_index_html += '    </ul class="url-list">'
-links_for_archive_list += '      </ul></li>\n    </ul class="archive">'
-
-with open("./blog/index.html") as file:
-    old_index_html_file = file.read()
-    new_index_html_file = re.sub("<ul class=\"url-list\">.*</ul class=\"url-list\">",links_for_index_html, old_index_html_file,flags=re.DOTALL)
-    new_index_html_file = re.sub("<ul class=\"archive\">.*</ul class=\"archive\">",links_for_archive_list, new_index_html_file,flags=re.DOTALL)
-with open("./blog/index.html", mode="w") as file:
-    file.write(new_index_html_file)
+SRC_DIR = Path("./src")
+OUTPUT_DIR = Path("./dist")
+TEMPLATE_DIR = Path("./templates")
 
 
-for category in ["./works/*/*.html","./blog/*/*.html"]:
-    files = glob.glob(category)
-    for url in files:
-        with open(url) as file:
-            nakami = file.read()
-            #titleタグからページ名を習得、頭についてる「りんりん - 」は外す
-            pagename = re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' ')))
+def remove_top_dir(p:Path):
+    return p.relative_to(p.parts[0])
 
-            if url[-10:] == "index.html":
-                shorturl = url[:-11]
-            else:
-                shorturl = url
-            links_for_index_html += "      <li><a href=\"" + shorturl + "\">" + pagename + "</a></li>\n"
-            metatag = "<!--置換用タグ1-->\n    <meta property=\"og:url\" content=\"https://lnln.dev/" + re.sub("\./","",shorturl) + "\">\n    <meta property=\"og:title\" content=\"" + pagename + "\">\n    <!--置換用タグ2-->"
+def convert_md():
+    markdown_obj = markdown.Markdown(extensions=['fenced_code'])
 
-            onew = re.sub("<!--置換用タグ1-->.*<!--置換用タグ2-->",metatag,nakami,flags=re.DOTALL)
-        with open(url,mode="w") as file:
-            file.write(onew)
+    # ls blog html files
+    md_files = glob.glob( str(SRC_DIR/"blog/*") )
+    #md_files = [f for f in md_files if os.path.isfile(f)]
+    md_files.sort(reverse=True)
 
-#   mkdir dist
-if not os.path.exists("./dist"):
-    os.mkdir("./dist")
+    for category in ["css", "works", "blog", "img"]:
+        files = glob.glob( str(SRC_DIR/category) )
+        for url in files:
+            url = Path(url)
+            url = remove_top_dir(url)
+            copytree(SRC_DIR/url, OUTPUT_DIR/url )
 
 
-for category in ["css", "works", "blog"]:
-    files = glob.glob(category)
-    for url in files:
-        copytree(url,"./dist/" + url)
+    for index, md_file_path in enumerate(md_files):
+        md_file_name = md_file_path[-10:] + ".md"
+        md_file_name = Path(md_file_name)
 
-copyfile("index.html","./dist/index.html")
-copyfile("404.html","./dist/404.html")
-copyfile("CNAME","./dist/CNAME")
+        with open(md_file_path/md_file_name) as file:
+            md_text = file.read()
+            html = markdown_obj.convert(md_text)
+
+            md_file_path = remove_top_dir( Path(md_file_path) )
+            
+            # htmlいいかんじにrつくう
+            with open(TEMPLATE_DIR/"blog-template.html", mode="r") as template:
+                html_template = template.read()
+                html = html_template.replace("<!--置換用タグ3-->",html)
+
+            with open(OUTPUT_DIR/md_file_path/"index.html", mode="w") as file:
+                #print(OUTPUT_DIR/md_file_path/md_file_name.with_suffix('.html'))
+                os.remove(OUTPUT_DIR/md_file_path/md_file_name.with_suffix('.md'))
+                file.write(html)
 
 
+def put_index_file():
+    # ls blog html files
+    html_files = glob.glob( str(OUTPUT_DIR/"blog/*") )
+    html_files.sort(reverse=True)
 
-print("Done!!")
-
+    links_for_index_html ='<ul class="url-list">\n'
+    links_for_archive_list = '<ul class="archive">\n'
+    first = True
     
+    for index, url in enumerate(html_files):
+        if url[-10:] == "index.html":
+            pass
+        else:
+            #ファイルを開いて
+            with open(url + "/index.html") as file:
+                url = remove_top_dir(Path(url))
+                url = remove_top_dir(url)
+                url = str(url)
+                nakami = file.read()
+
+                # get title
+                pagename = re.sub("</h1>.*", "",re.sub(".*<h1>","",nakami.replace('\n',' ')))
+                date = url[-10:]
+                date = re.sub("-","/",date)
+
+                if index < NUMBER_OF_LATEST_BLOG:
+                    links_for_index_html += f'      <li><a href="./{url}">{pagename} - {date}</a></li>\n'
+
+                if first:
+                    links_for_archive_list += f'      <details><summary>{date[:-3]}</summary>\n        <li><a href="./{url}">{pagename} - {date}</a></li>\n'
+                    first = False
+                elif former != date[:-3]:
+                    links_for_archive_list += f'      </details>\n      <details><summary>{date[:-3]}</summary>\n        <li><a href="./{url}">{pagename} - {date}</a></li>\n'
+                else:
+                    links_for_archive_list += f'        <li><a href="./{url}">{pagename} - {date}</a></li>\n'
+                former = date[:-3]
+
+    links_for_index_html += '    </ul class="url-list">'
+    links_for_archive_list += '      </ul></li>\n    </ul class="archive">'
+
+    with open(TEMPLATE_DIR/"blog-index-template.html") as file:
+        old_index_html_file = file.read()
+
+        new_index_html_file = re.sub('<ul class="url-list">.*</ul class="url-list">',links_for_index_html, old_index_html_file,flags=re.DOTALL)
+        new_index_html_file = re.sub('<ul class="archive">.*</ul class="archive">',links_for_archive_list, new_index_html_file,flags=re.DOTALL)
+
+    with open(OUTPUT_DIR/"blog/index.html", mode="w") as file:
+        file.write(new_index_html_file)
+
+
+def edit_url_and_title():
+    for category in ["./works/*/*.html","./blog/*/*.html"]:
+        files = glob.glob( str(OUTPUT_DIR/category) )
+        
+        for url in files:
+            with open(url) as file:
+                nakami = file.read()
+
+                pagename = re.sub("</h1>.*", "",re.sub(".*<h1>","",nakami.replace('\n',' ')))
+
+                if url[-10:] == "index.html":
+                    shorturl = url[:-11]
+                else:
+                    shorturl = url
+
+                metatag = "\n    <meta property=\"og:url\" content=\"https://lnln.dev/" + re.sub("\./","",shorturl) \
+                    + "\">\n    <meta property=\"og:title\" content=\"" + pagename + "\">\n"\
+                    + "    <title>" + pagename + "</title>"
+
+                onew = re.sub("<!--置換用タグ1-->.*<!--置換用タグ2-->",metatag,nakami,flags=re.DOTALL)
+
+            
+            with open( url, mode="w" ) as file:
+                file.write(onew)
+
+
+if __name__ == "__main__":
+    #   mkdir dist
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    
+    convert_md()
+    put_index_file()
+
+    edit_url_and_title()
+
+    for file in ["404.html", "CNAME", "index.html"]:
+        copyfile( SRC_DIR/file, OUTPUT_DIR/file )
