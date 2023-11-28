d22dfe630e4e7e230011ad4e366a1acb5022f9ce
ybasviel <67199957+ybasviel@users.noreply.github.com>
2023-11-28 18:01:34 +0900

    revive blog

--- make.py
+++ make.py
@@ -2,53 +2,53 @@
 
 import glob
 import re
-import sys
 
-#blogの分
+NUMBER_OF_LATEST_BLOG = 3
 
-linksforindex ="<ul class=\"url-list\">\n"
-linksforarchive = "<ul class=\"archive\">\n"
+links_for_index_html ='<ul class="url-list">\n'
+links_for_archive_list = '<ul class="archive">\n'
 first = True
 
-htmlfiles = glob.glob("./blog/*")
-htmlfiles.sort(reverse=True)
+# ls blog html files
+html_files = glob.glob("./blog/*")
+html_files.sort(reverse=True)
 
-latest3 = 0 #最新の3件のみ表示
+#latest3 = 0 #最新の3件のみ表示
 
-for url in htmlfiles:
+for index, url in enumerate(html_files):
     if url[-10:] == "index.html":
         pass
     else:
         #ファイルを開いて
         with open(url + "/index.html") as file:
             nakami = file.read()
-            #titleタグからページ名を習得、頭についてる「りんりん - 」は外す
-            pagename = re.sub("りんりん - ","",re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' '))))
-            date = url[-10:] #re.sub("</div class=\"date\">.*", "",re.sub(".*<div class=\"date\">","",nakami.replace('\n',' ')))
+
+            # get title
+            pagename = re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' ')))
+            date = url[-10:]
             date = re.sub("-","/",date)
 
-            if latest3 < 3:
-                linksforindex += "      <li><a href=\"." + url + "\">" + pagename + " - " + date + "</a></li>\n"
-                latest3 = latest3 + 1
+            if index <= NUMBER_OF_LATEST_BLOG:
+                links_for_index_html += f'      <li><a href=".{url}">{pagename} - {date}</a></li>\n'
 
             if first:
-                linksforarchive += "      <details><summary>" + date[:-3] + "</summary>\n        <li><a href=\"." + url + "\">" + pagename + " - " + date + "</a>" + "</li>\n"
+                links_for_archive_list += f'      <details><summary>{date[:-3]}</summary>\n        <li><a href=".{url}">{pagename} - {date}</a></li>\n'
                 first = False
             elif former != date[:-3]:
-                linksforarchive += "      </details>\n      <details><summary>" + date[:-3] + "</summary>\n        <li><a href=\"." + url + "\">" + pagename + " - " + date + "</a>" + "</li>\n"
+                links_for_archive_list += f'      </details>\n      <details><summary>{date[:-3]}</summary>\n        <li><a href=".{url}">{pagename} - {date}</a></li>\n'
             else:
-                linksforarchive += "        <li><a href=\"." + url + "\">" + pagename + " - " + date + "</a>" + "</li>\n"
+                links_for_archive_list += f'        <li><a href=".{url}">{pagename} - {date}</a></li>\n'
             former = date[:-3]
 
-linksforindex += "    </ul class=\"url-list\">"
-linksforarchive += "      </ul></li>\n    </ul class=\"archive\">"
+links_for_index_html += '    </ul class="url-list">'
+links_for_archive_list += '      </ul></li>\n    </ul class="archive">'
 
 with open("./blog/index.html") as file:
-    nakami = file.read()
-    onew = re.sub("<ul class=\"url-list\">.*</ul class=\"url-list\">",linksforindex, nakami,flags=re.DOTALL)
-    onew = re.sub("<ul class=\"archive\">.*</ul class=\"archive\">",linksforarchive, onew,flags=re.DOTALL)
+    old_index_html_file = file.read()
+    new_index_html_file = re.sub("<ul class=\"url-list\">.*</ul class=\"url-list\">",links_for_index_html, old_index_html_file,flags=re.DOTALL)
+    new_index_html_file = re.sub("<ul class=\"archive\">.*</ul class=\"archive\">",links_for_archive_list, new_index_html_file,flags=re.DOTALL)
 with open("./blog/index.html", mode="w") as file:
-    file.write(onew)
+    file.write(new_index_html_file)
 
 
 for category in ["./works/*/*.html","./blog/*/*.html"]:
@@ -57,13 +57,13 @@ for category in ["./works/*/*.html","./blog/*/*.html"]:
         with open(url) as file:
             nakami = file.read()
             #titleタグからページ名を習得、頭についてる「りんりん - 」は外す
-            pagename = re.sub("りんりん - ","",re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' '))))
+            pagename = re.sub("</title>.*", "",re.sub(".*<title>","",nakami.replace('\n',' ')))
 
             if url[-10:] == "index.html":
                 shorturl = url[:-11]
             else:
                 shorturl = url
-            linksforindex += "      <li><a href=\"" + shorturl + "\">" + pagename + "</a></li>\n"
+            links_for_index_html += "      <li><a href=\"" + shorturl + "\">" + pagename + "</a></li>\n"
             metatag = "<!--置換用タグ1-->\n    <meta property=\"og:url\" content=\"https://lnln.dev/" + re.sub("\./","",shorturl) + "\">\n    <meta property=\"og:title\" content=\"" + pagename + "\">\n    <!--置換用タグ2-->"
 
             onew = re.sub("<!--置換用タグ1-->.*<!--置換用タグ2-->",metatag,nakami,flags=re.DOTALL)
@@ -71,3 +71,5 @@ for category in ["./works/*/*.html","./blog/*/*.html"]:
             file.write(onew)
 
 print("Done!!")
+
+    
